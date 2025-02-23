40ada1e7a74fe5c0a645ffe9142136daabdba44b
Luis Roberto Bastos <Luis Roberto Bastos@SAP2>
2025-02-23 12:08:05 -0600

    Ajustes para adaptar estabilizar el sistema

--- Play/Class/SAP_BUSSINES_ONE.vb
+++ Play/Class/SAP_BUSSINES_ONE.vb
@@ -1089,7 +1089,7 @@ Public Class SAP_BUSSINES_ONE
 
                 'PARAMETROS DE CONEXION A SAP
                 oCompany = New SAPbobsCOM.Company
-                oCompany.DbServerType = SAPbobsCOM.BoDataServerTypes.dst_MSSQL2019
+                oCompany.DbServerType = SAPbobsCOM.BoDataServerTypes.dst_MSSQL2014
 
                 If Class_VariablesGlobales.XMLParamSAP_CompanyDB.Trim <> "" Then
                     oCompany.CompanyDB = Class_VariablesGlobales.XMLParamSAP_CompanyDB.Trim
